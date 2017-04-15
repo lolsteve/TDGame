43d123419e5659779df1ce79c3a1adc2917bb7b2
Steven Wallace <steven@thewallaces.ca>
2017-04-15 15:43:31 -0700

    Initial commit

---
+++ Assets/Enemy.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+	GameObject pathGO;
+
+	Transform targetPathNode;
+	int pathNodeIndex = 0;
+
+	float speed = 2f;
+
+	public int health = 1;
+
+	// Use this for initialization
+	void Start () {
+		pathGO = GameObject.Find ("Path");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (targetPathNode == null) {
+			GetNextPathNode ();
+			if (targetPathNode == null) {
+				// end of path?
+				ReachedGoal();
+				return;
+			}
+		}
+
+		Vector2 dir = targetPathNode.position - this.transform.localPosition;
+
+		float distThisFrame = speed * Time.deltaTime;
+
+		if (dir.magnitude <= distThisFrame) {
+			// Reached target node
+			targetPathNode = null;
+		} else {
+			// Move enemy
+			this.transform.Translate( dir.normalized * distThisFrame, Space.World );
+		}
+
+
+	}
+
+	void GetNextPathNode () {
+		if (pathNodeIndex >= pathGO.transform.childCount) {
+			ReachedGoal ();
+		} else {
+			targetPathNode = pathGO.transform.GetChild (pathNodeIndex++);
+		}
+	}
+
+	void ReachedGoal () {
+		Destroy (gameObject);
+	}
+}
---
+++ Assets/Tower.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour {
+
+	Transform turretTransform;
+
+	// Use this for initialization
+	void Start () {
+		turretTransform = transform.Find ("Turret");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Enemy[] enemies = GameObject.FindObjectsOfType<Enemy>();
+
+		Enemy nearestEnemy = null;
+		float dist = Mathf.Infinity;
+
+		foreach (Enemy e in enemies) {
+			float d = Vector2.Distance (this.transform.position, e.transform.position);
+			if (nearestEnemy == null || d < dist) {
+				nearestEnemy = e;
+				dist = d;
+			}
+		}
+
+		if (nearestEnemy == null) {
+			Debug.Log ("No enemies");
+			return;
+		}
+
+		Vector3 dir = nearestEnemy.transform.position - this.transform.position;
+
+		Debug.Log (dir);
+
+		Quaternion lookRot = Quaternion.LookRotation (dir, Vector3.up);
+
+		// This is weird
+		if (lookRot.eulerAngles.y == 270) {
+			turretTransform.rotation = Quaternion.Euler (0, 0, lookRot.eulerAngles.x + 90f);
+		} else {
+			turretTransform.rotation = Quaternion.Euler (0, 0, -lookRot.eulerAngles.x - 90f);
+		}
+	}
+}
